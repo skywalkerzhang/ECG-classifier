4fa310c0ece88fab2f08e44776998742579ab081
dragondove <jinxiongrong@foxmail.com>
2021-07-14 23:04:23 +0800

    update qrs interval

--- QRS_interval.py
+++ QRS_interval.py
@@ -1,56 +1,50 @@
 import neurokit2 as nk
 import numpy as np
-from wfdb import processing
-from sklearn import svm
-import pandas as pd
-import matplotlib.pyplot as plt
 import math
-import seaborn as sns
-
-from helper_code import load_recording
-
-LAD_r_peaks = []
-LAD_waves_dwt = []
-LAD_interval = []
 
 
 # import data: train split & test split
 
 # feature selection
 # get all sum of QRS
-leads = {0: 'I', 1: 'II', 2: 'III', 3: 'aVR', 4: 'aVL', 5: 'aVF', 6: 'V1', 7: 'V2', 8: 'V3', 9: 'V4', 10: 'V5', 11: 'V6'}
+leads = {0: 'I', 1: 'II', 2: 'III', 3: 'aVR',
+        4: 'aVL', 5: 'aVF', 6: 'V1', 7: 'V2',
+        8: 'V3', 9: 'V4', 10: 'V5', 11: 'V6'}
 # get the average length of interval
 
 # Retrieve ECG data from data folder (sampling rate= 1000 Hz)
-LAD_recordings = np.load('../data/training_sets0.npy')
-for x in LAD_recordings:
-    print('I am running')
-    each_peaks = []
-    each_waves = []
-    each_inter = []
-    for i in leads.keys():
-        print(i)
-        ecg_signal = x[i]
+LAD_recordings = np.load('../data/test_sets0.npy')
+print('process data, data size: ', len(LAD_recordings))
+LAD_features = []
+for recording in LAD_recordings:
+    print('running', len(LAD_features))
+    # store features of 12 leads
+    leads_features = []
+    for lead_idx in leads.keys():
+        # store three features
+        lead_features = []
+        ecg_signal = recording[lead_idx]
+
         # Extract R-peaks locations
         # clean ecg
         ecg_signal = nk.ecg_clean(ecg_signal, 257)
+        # get r peaks of a signal
         r_peaks = None
         try:
             _, r_peaks = nk.ecg_peaks(ecg_signal, sampling_rate=257)
             if len(r_peaks['ECG_R_Peaks']) == 0:
-                each_inter.append(0)
-                each_inter.append(0)
-                nk.signal_plot(ecg_signal, 257)
-                print(idx, 'wrong')
-                plt.show()
+                lead_features.append(0)
+                lead_features.append(0)
+                leads_features.append(lead_features)
                 continue
         except:
-            nk.signal_plot(ecg_signal, 257)
-            print(idx)
-            plt.show()
+            lead_features.append(0)
+            lead_features.append(0)
+            leads_features.append(lead_features)
+            continue
+
         # 返回数据下标
         # r_peaks = processing.xqrs_detect(ecg_signal, fs)
-        each_peaks.append(r_peaks)
         # # Visualize R-peaks in ECG signal
         # plt.figure()
         # plt.plot(ecg_signal)
@@ -65,8 +59,9 @@ for x in LAD_recordings:
         signal_dwt, waves_dwt = nk.ecg_delineate(ecg_signal, r_peaks, sampling_rate=257, method="dwt", show_type='bounds_R')
         R_Onsets = waves_dwt['ECG_R_Onsets']
         R_Offsets = waves_dwt['ECG_R_Offsets']
-        s = 0
+
         # the sum of QRS
+        s = 0
         for j in range(len(R_Onsets)):
             if math.isnan(R_Onsets[j]):
                 R_Onsets[j] = 0
@@ -75,13 +70,12 @@ for x in LAD_recordings:
             for k in range(int(R_Onsets[j]), int(R_Offsets[j]) + 1):
                 s += ecg_signal[k]
 
-        each_inter.append(s)
-        each_inter.append(sum(R_Offsets) - sum(R_Onsets) / len(R_Onsets))
-
-        # the average length of QRS
-
+        lead_features.append(s)
+        lead_features.append(sum(R_Offsets) - sum(R_Onsets) / len(R_Onsets))
         
-    LAD_r_peaks.append(each_peaks)
-    LAD_interval.append(each_inter)
+        leads_features.append(lead_features)
 
-np.save('../data/LAD_train.npy', np.array(LAD_interval))
+    LAD_features.append(leads_features)
+        
+
+np.save('../data/LAD_train.npy', np.array(LAD_features))
